667a223c58e794adc192f680c461242e789fd0d4
kemiisto <kemiisto@gmail.com>
2022-05-30 18:27:53 +0300

    Add clear window example

---
+++ src/01_getting_started/01_02_hello_window_clear/hello_window_clear.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <tinygl/tinygl.h>
+#include <iostream>
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void processInput() override;
+    void draw() override;
+};
+
+void Window::processInput()
+{
+    if (getKey(tinygl::Key::Escape) == tinygl::KeyState::Press) {
+        setShouldClose(true);
+    }
+}
+
+void Window::draw()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+MAIN
