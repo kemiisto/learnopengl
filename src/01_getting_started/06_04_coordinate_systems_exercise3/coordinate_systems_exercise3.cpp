17db19e787ac1ac3f6160f0804affebc4abda7d0
kemiisto <kemiisto@gmail.com>
2022-06-21 19:08:31 +0300

    Add coordinate systems exercise 3

---
+++ src/01_getting_started/06_04_coordinate_systems_exercise3/coordinate_systems_exercise3.cpp
@@ -0,0 +1,156 @@
+#include "main.h"
+#include "resource.h"
+#include <tinygl/tinygl.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+class Window final : public tinygl::Window
+{
+public:
+    using tinygl::Window::Window;
+    void init() override;
+    void processInput() override;
+    void draw() override;
+private:
+    tinygl::ShaderProgram program;
+    tinygl::Buffer vbo{tinygl::Buffer::Type::VertexBuffer, tinygl::Buffer::UsagePattern::StaticDraw};
+    tinygl::VertexArrayObject vao;
+    tinygl::Texture texture0{
+        tinygl::Texture::Target::Target2D,
+        resourcePath("textures/container.jpg"),
+        GL_RGB, GL_RGB, true, 0
+    };
+    tinygl::Texture texture1{
+        tinygl::Texture::Target::Target2D,
+        resourcePath("textures/awesomeface.png"),
+        GL_RGBA, GL_RGBA, true, 1
+    };
+};
+
+void Window::init()
+{
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Vertex, "coordinate_systems_exercise3.vert");
+    program.addShaderFromSourceFile(tinygl::Shader::Type::Fragment, "coordinate_systems_exercise3.frag");
+    program.link();
+
+    program.use();
+    program.setUniformValue("texture0", 0);
+    program.setUniformValue("texture1", 1);
+
+    const GLfloat vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    vbo.bind();
+    vbo.fill(vertices, sizeof(vertices));
+    vbo.unbind();
+
+    vao.bind();
+
+    vbo.bind();
+    vao.setAttributeArray(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+    vao.enableAttributeArray(0);
+    vao.setAttributeArray(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 3 * sizeof(GLfloat));
+    vao.enableAttributeArray(1);
+    vbo.unbind();
+
+    vao.unbind();
+
+    glEnable(GL_DEPTH_TEST);
+}
+
+void Window::processInput()
+{
+    if (getKey(tinygl::Key::Escape) == tinygl::KeyState::Press) {
+        setShouldClose(true);
+    }
+}
+
+void Window::draw() {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    texture0.bind();
+    texture1.bind();
+
+    const glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+    program.use();
+    vao.bind();
+
+    auto view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    program.setUniformValue("view", view);
+
+    auto projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+    program.setUniformValue("projection", projection);
+
+    for (int i = 0; i < 10; ++i) {
+        auto model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        if (i % 3 == 0) {
+            model = glm::rotate(model, glm::radians(tinygl::getTime() * 50.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+        } else {
+            model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+        }
+
+        program.setUniformValue("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+}
+
+MAIN
+
