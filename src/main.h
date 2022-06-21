9cf8bc1faa732c87d4a253957ee54fcf8636dd2b
kemiisto <kemiisto@gmail.com>
2022-06-21 14:53:55 +0300

    Move tinygl

--- src/main.h
+++ src/main.h
@@ -5,10 +5,11 @@
 int main()                                       \
 {                                                \
     try {                                        \
+        tinygl::init(3, 3);                      \
         Window window(800, 600, NAME);           \
         window.run();                            \
     } catch (const std::exception& e) {          \
-        glfwTerminate();                         \
+        tinygl::terminate();                     \
         std::cerr << e.what() << std::endl;      \
         return EXIT_FAILURE;                     \
     }                                            \
